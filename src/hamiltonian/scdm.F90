32d96a5aef27bd72ea31d9642a03c4884a0e1aea
hhuebener <hannes.huebener@gmail.com>
2014-12-13 14:37:42 +0100

    try to add two files

---
+++ src/hamiltonian/scdm.F90
@@ -0,0 +1,354 @@
+#include "global.h"
+
+module scdm_m
+! module list is copied from hamiltonian
+  use batch_m
+  use batch_ops_m
+  use blas_m
+#ifdef HAVE_OPENCL
+  use cl
+#endif
+  use cmplxscl_m
+  use cube_m
+  use cube_function_m
+  use datasets_m
+  use derivatives_m
+  use energy_m
+!  use hamiltonian_base_m
+!  use epot_m
+!  use gauge_field_m
+  use fft_m
+  use nfft_m
+  use geometry_m
+  use global_m
+  use grid_m
+  use hardware_m
+  use io_m
+  use io_function_m
+  use kpoints_m
+  use lalg_basic_m
+  use lasers_m
+  use math_m
+  use mesh_m
+  use mesh_cube_map_m
+  use mesh_function_m
+  use messages_m
+  use mpi_m
+  use mpi_lib_m
+  use multicomm_m
+  use opencl_m
+  use ob_interface_m
+!  use ob_lead_m
+  use opencl_m
+  use par_vec_m
+  use parser_m
+  use poisson_m
+  use poisson_fft_m
+  use profiling_m
+!  use projector_m
+  use simul_box_m
+  use smear_m
+  use states_m
+  use states_dim_m
+  use types_m
+  use unit_m
+  use unit_system_m
+  use varinfo_m
+  use xc_m
+  use XC_F90(lib_m)
+
+  implicit none
+
+  private
+  public ::               &
+       scdm_t,            &
+       scdm_init,         &
+       dscdm_localize,    &
+       zscdm_localize
+  
+  type scdm_t
+     type(states_t)   :: st          ! localized orthogonal states
+     type(poisson_t)  :: poisson1     ! solver
+     type(cube_t)     :: cube        ! mesh cube for fft
+     FLOAT, pointer   :: center(:,:) ! coordinates of centers of states (in same units as mesh%x)
+     FLOAT            :: rcut        ! orbital cutoff radius (box size)                                       NOTE: this could be dynamic and state dependent
+     integer          :: box_size    ! number of mesh points in the dimension of local box around scdm states NOTE: this could be dynamic and state dependent
+     integer          :: full_box    ! = (2*(2*box_size+1))**3
+     type(mesh_t)     :: boxmesh     ! mesh describing the small box
+     type(cube_t)     :: boxcube     ! cube of the small box (used for fft in poisson solver, has doubled size for truncation)
+     integer, pointer :: box(:,:,:,:)  ! indices of global points that are contained in the local box for each state NOTE: this should be done smarter
+     FLOAT, pointer   :: dpsi(:,:)   ! scdm states in their local box
+     CMPLX, pointer   :: zpsi(:,:)   ! .
+     type(poisson_t)  :: poisson     ! solver used to compute exchange with localized scdm states
+     type(poisson_fft_t) :: poisson_fft ! used for above poisson solver
+     type(cmplxscl_t)    :: cmplxscl
+     !
+     ! parallelization of scdm states
+     logical          :: root        ! this is a redundat flag equal to mesh%vp%rank==0
+     integer          :: nst
+     integer          :: st_start    ! the distributed index
+     integer          :: st_end      ! .
+     integer          :: lnst        ! .
+     !
+! debug stuff
+     FLOAT, pointer  :: rho(:)   ! density built from SCDM
+     FLOAT, pointer  :: pot(:)
+     logical         :: has_hartree
+     integer         :: iter
+  end type scdm_t
+  !
+  logical,public    :: scdm_is_init=.false.  ! is initialized
+  logical,public    :: scdm_is_local=.false.  ! is localized
+  !
+  type(scdm_t), public       :: scdm
+! debug stuff
+  type(geometry_t), public   :: scdm_geo
+  !
+  contains
+
+  subroutine scdm_init(st,der,scdm)
+    !
+    type(states_t), intent(in)  :: st ! this contains the KS set (for now from hm%hf_st which is confusing)
+    type(derivatives_t) :: der
+    type(scdm_t) :: scdm
+    type(cmplxscl_t) :: cmplxscl
+    !
+    integer :: i,j,k, ip
+    !debug
+    integer :: temp(3)
+    !
+    integer,  allocatable:: istart(:)
+    integer,  allocatable:: iend(:)
+    integer,  allocatable:: ilsize(:)
+    !
+    ! check if already initialized
+    if(scdm_is_init) return
+    !
+    if(st%lnst.ne.st%nst) call messages_not_implemented("SCDM with state parallelization")
+    !
+    scdm%root = (der%mesh%vp%rank == 0)
+    !
+    ! inherit some indices from st
+    !scdm%st%d%dim = st%d%dim
+    scdm%st%nst   = st%nst
+    scdm%nst   = st%nst
+    scdm%st%d%nik = st%d%nik
+    scdm%st%d     = st%d
+    scdm%cmplxscl = st%cmplxscl
+    !
+    !
+    ! allocate centers
+    SAFE_ALLOCATE(scdm%center(3,scdm%st%nst))
+    !
+    ! make a cube around the center points
+    ! with side length NOTE: this should be dynamic
+!    call parse_float(datasets_check('SCDMCutoffRadius'), 3., scdm%rcut, units_inp%length)
+    scdm%rcut = 3.
+    ! box_size is half the size of the  box
+    scdm%box_size = 0
+    do i=1,3
+       scdm%box_size = max(scdm%box_size,ceiling(scdm%rcut/der%mesh%spacing(i)))
+    enddo
+    if(scdm%root) then
+       print *, 'HH: SCDM box_size', scdm%box_size
+       print *, 'HH: SCDM box_size[Ang]', scdm%box_size*der%mesh%spacing(1)*0.529177249
+    endif
+    scdm%full_box = (2*(2*scdm%box_size+1))**3
+    if(scdm%root) print *, 'HH: SCDM fullbox[Ang]', 2*(2*scdm%box_size+1)*der%mesh%spacing(1)*0.529177249
+    SAFE_ALLOCATE(scdm%box(scdm%box_size*2+1,scdm%box_size*2+1,scdm%box_size*2+1,scdm%st%nst))
+    !
+    ! the localzied states defined in the box are distributed over state index
+    SAFE_ALLOCATE(istart(der%mesh%mpi_grp%size))
+    SAFE_ALLOCATE(iend(der%mesh%mpi_grp%size))
+    SAFE_ALLOCATE(ilsize(der%mesh%mpi_grp%size))
+    !
+    call multicomm_divide_range(st%nst,der%mesh%mpi_grp%size, istart, iend, lsize=ilsize)
+    scdm%st_start = istart(der%mesh%vp%rank+1)
+    scdm%st_end = iend(der%mesh%vp%rank+1)
+    scdm%lnst = ilsize(der%mesh%vp%rank+1)
+    !
+    ! allocate local chunk of states
+    ! localized SCDM states in full box (not really needed, but convenient for distribution)
+    ! root process holds all states NOTE: this is not great... clearly ... but will go away when SCDM procedure is parallel
+    if(.not.states_are_real(st)) then
+       if(scdm%root) then
+          SAFE_ALLOCATE(scdm%st%zpsi(der%mesh%np_part_global, scdm%st%d%dim, scdm%st%nst, scdm%st%d%nik))
+       else
+          SAFE_ALLOCATE(scdm%st%zpsi(der%mesh%np_part_global, scdm%st%d%dim, scdm%lnst, scdm%st%d%nik))
+
+       endif
+       ! localized SCDM states defined on box twice their size for coulomb truncation
+       SAFE_ALLOCATE(scdm%zpsi(scdm%full_box,scdm%lnst))
+    else ! real
+       if(scdm%root) then
+          SAFE_ALLOCATE(scdm%st%dpsi(der%mesh%np_part_global, scdm%st%d%dim, scdm%st%nst, scdm%st%d%nik))
+       else
+          SAFE_ALLOCATE(scdm%st%dpsi(der%mesh%np_part_global, scdm%st%d%dim, scdm%lnst, scdm%st%d%nik))
+
+       endif
+       ! localized SCDM states defined on box twice their size for coulomb truncation
+       SAFE_ALLOCATE(scdm%dpsi(scdm%full_box,scdm%lnst))
+
+    endif
+    !
+    ! create a mesh object for the small box (for now each scdm state is in the same box, should be dynamic)
+    ! only initialize values needed in the following (e.g. by poisson_fft_init)
+    scdm%boxmesh%spacing(:) = minval(der%mesh%spacing(:))
+    SAFE_ALLOCATE(scdm%boxmesh%sb)
+    scdm%boxmesh%sb%periodic_dim = 0
+    scdm%boxmesh%sb%dim = 3
+    scdm%boxmesh%sb%klattice_primitive(:,:) = reshape((/1.,0.,0.,0.,1.,0.,0.,0.,1./),(/3,3/))
+    scdm%boxmesh%sb%rlattice_primitive(:,:) = reshape((/1.,0.,0.,0.,1.,0.,0.,0.,1./),(/3,3/))
+!
+    !set mesh points with double size for coulomb truncation
+    scdm%boxmesh%np = scdm%full_box
+    scdm%boxmesh%np_global = scdm%boxmesh%np
+    scdm%boxmesh%np_part = scdm%boxmesh%np_global
+    scdm%boxmesh%np_part_global = scdm%boxmesh%np_global
+    ! set index type of mesh
+    scdm%boxmesh%idx%is_hypercube = .false.
+    ! mesh has to be centered around zero with left overhang otherwise mesh_cub_map doesn't seem to work
+    scdm%boxmesh%idx%nr(1,:) = -(scdm%box_size*2+1)
+    scdm%boxmesh%idx%nr(2,:) =  (scdm%box_size*2+1)-1
+    !
+    scdm%boxmesh%idx%dim = 3
+    scdm%boxmesh%idx%ll(:) = scdm%boxmesh%idx%nr(2,:) - scdm%boxmesh%idx%nr(1,:) + 1
+    scdm%boxmesh%idx%enlarge(:) = 0
+    SAFE_ALLOCATE(scdm%boxmesh%idx%lxyz(scdm%boxmesh%np,scdm%boxmesh%idx%dim))
+    ! need to copy indices because otherwise line gets too long (???)
+    i=-(scdm%box_size*2+1)
+    j=(scdm%box_size*2+1)-1
+    SAFE_ALLOCATE(scdm%boxmesh%idx%lxyz_inv(i:j,i:j,i:j))
+
+    !
+    ip = 0
+    do i=scdm%boxmesh%idx%nr(1,1),scdm%boxmesh%idx%nr(2,1)
+       do j=scdm%boxmesh%idx%nr(1,2),scdm%boxmesh%idx%nr(2,2)
+          do k=scdm%boxmesh%idx%nr(1,3),scdm%boxmesh%idx%nr(2,3)
+             ip = ip +1
+             scdm%boxmesh%idx%lxyz(ip,1) = i
+             scdm%boxmesh%idx%lxyz(ip,2) = j
+             scdm%boxmesh%idx%lxyz(ip,3) = k
+             scdm%boxmesh%idx%lxyz_inv(i,j,k) = ip
+          enddo
+       enddo
+    enddo
+    call mesh_cube_map_init(scdm%boxmesh%cube_map, scdm%boxmesh%idx, scdm%boxmesh%np_global)
+    !
+    ! create a cube object for the small box, with double size for coulomb truncation
+    ! instead,this should be used to enlarge the box, but then need to keep track of dimension:
+    !call mesh_double_box(scdm%boxmesh%sb, scdm%boxmesh, 2._8, temp)
+    !
+    call cube_init(scdm%boxcube, scdm%boxmesh%idx%ll, scdm%boxmesh%sb, &
+               fft_type=FFT_REAL, fft_library=FFTLIB_FFTW, dont_optimize = .true.)
+
+    ! Joseba recommends including this
+    !if (der%mesh%parallel_in_domains .and. this%cube%parallel_in_domains) then
+    !    call mesh_cube_parallel_map_init(this%mesh_cube_map, der%mesh, this%cube)
+    !end if
+
+    !
+    ! set up poisson solver used for the exchange operator with scdm states
+    ! this replictaes poisson_kernel_init()
+    scdm%poisson%poisson_soft_coulomb_param = M_ZERO
+    call poisson_fft_init(scdm%poisson_fft, scdm%boxmesh, scdm%boxcube, kernel=POISSON_FFT_KERNEL_SPH)
+    !
+    ! create poisson object
+    SAFE_ALLOCATE(scdm%poisson%der)
+    SAFE_ALLOCATE(scdm%poisson%der%mesh)
+    scdm%poisson%der%mesh = scdm%boxmesh
+    scdm%poisson%method = POISSON_FFT
+    scdm%poisson%kernel = POISSON_FFT_KERNEL_SPH
+    scdm%poisson%cube = scdm%boxcube
+    scdm%poisson%fft_solver = scdm%poisson_fft
+    !
+    ! set flag to do this only once
+    scdm_is_init = .true.
+    !
+    print *, 'HH:  done SCDM init'
+    !
+  end subroutine scdm_init
+
+  subroutine dRRQR(n,np,KSt,JPVT)
+    ! wrapper routine for real rank-revealing QR decompisition
+    ! of the n*np matrix KSt, returning the pivot vector JPVT
+    !
+    integer, intent(in)  :: np,n
+    FLOAT, intent(inout) :: KSt(:,:)
+    integer, intent(out) :: JPVT(np)
+    !
+    integer            :: lwork,INFO
+    FLOAT              :: TAU(n)
+    FLOAT, allocatable ::  work(:)
+    !
+    ! dummy call to obtain dimension of work
+    allocate(work(1))
+    call DGEQP3( n,np, KSt, n, JPVT, TAU, WORK, -1, INFO )
+    if(INFO.ne.0) then
+       print *, 'Illegal argument in DGEQP3: ', INFO
+       stop
+    endif
+    ! Note: scalapack routine is called P?GEQPF()
+    !
+    lwork = work(1)
+    deallocate(work)
+    allocate(work(lwork))
+    !
+    JPVT(:) = 0
+    TAU(:) = 0.
+    ! actual call
+    call DGEQP3( n,np, KSt, n, JPVT, TAU, WORK, LWORK, INFO )
+    if(INFO.ne.0)then
+       print *, 'Illegal argument in DGEQP3: ', INFO
+       stop
+    endif
+    !
+  end subroutine dRRQR
+
+  subroutine zRRQR(n,np,KSt,JPVT)
+    ! wrapper routine for complex rank-revealing QR decompisition
+    ! of the n*np matrix KSt, returning the pivot vector JPVT
+    !
+    integer, intent(in)  :: np,n
+    CMPLX, intent(inout) :: KSt(:,:)
+    integer, intent(out) :: JPVT(np)
+    !
+    integer            :: lwork,INFO
+    CMPLX              :: TAU(n)
+    CMPLX, allocatable :: work(:)
+    FLOAT              :: rwork(2*np)
+    !
+    ! dummy call to obtain dimension of work
+    allocate(work(1))
+    call ZGEQP3( n,np, KSt, n, JPVT, TAU, WORK, -1, RWORK, INFO )
+    if(INFO.ne.0) then
+       print *, 'Illegal argument in ZGEQP3: ', INFO
+       stop
+    endif
+    ! Note: scalapack routine is called P?GEQPF()
+    !
+    lwork = work(1)
+    deallocate(work)
+    allocate(work(lwork))
+    !
+    JPVT(:) = 0
+    TAU(:) = 0.
+    ! actual call
+    call ZGEQP3( n,np, KSt, n, JPVT, TAU, WORK, LWORK, RWORK, INFO )
+    if(INFO.ne.0)then
+       print *, 'Illegal argument in ZGEQP3: ', INFO
+       stop
+    endif
+    !
+  end subroutine zRRQR
+
+
+#include "undef.F90"
+#include "real.F90"
+#include "scdm_inc.F90"
+
+#include "undef.F90"
+#include "complex.F90"
+#include "scdm_inc.F90"
+
+end module scdm_m
---
+++ src/hamiltonian/scdm_inc.F90
@@ -0,0 +1,335 @@
+  subroutine X(scdm_localize)(st,mesh,scdm)
+    !
+    ! this performs the SCDM localization, transforming the original set of states KS (Kohn-Sham)
+    ! into the set SCDM, by first performing RRQR and then Cholesky for orthogonalization
+    !
+    type(states_t), intent(in)   :: st ! this contains the non-localize set KS (for now from hm%hf_st which is confusing)
+    type(mesh_t), intent(in)     :: mesh
+    type(scdm_t) :: scdm
+    !
+    integer            :: i,j,k,l,v,count,ip, nval, INFO
+    integer            ::JPVT(mesh%np_global)
+    integer            :: icenter(3), ind_center
+    !
+integer :: nn(3)
+    R_TYPE, allocatable :: KSt(:,:), KSt_original(:,:)
+    R_TYPE, allocatable ::  SCDM_temp(:,:), Pcc(:,:)
+    FLOAT, allocatable :: rho(:), pot(:), rho2(:)
+    FLOAT  :: exx
+    R_TYPE, allocatable     :: state_global(:), temp_state(:,:)
+    !
+!debug
+FLOAT :: t0,t1, t2, t3, t4
+CMPLX, allocatable :: mesh1(:,:,:), mesh2(:,:,:)
+FLOAT :: temp(3)
+character(len=50) :: name
+type(cube_function_t) :: cf
+    !
+    ! check if already localized
+    if(scdm_is_local) return
+    !
+call cpu_time(t0)
+    if(st%lnst.ne.st%nst) call messages_not_implemented("SCDM with state parallelization")
+    nval = st%nst ! TODO: check that this is really the number of valence states
+    !
+    ! built transpose of KS set on which RRQR is performed
+    if(scdm%root) then
+       SAFE_ALLOCATE(KSt(nval,mesh%np_global))
+       ! keep a copy of this NOTE: maybe too expensive in memory?
+       SAFE_ALLOCATE(KSt_original(nval,mesh%np_global))
+    endif
+    !
+    !NOTE: not sure how to proceed if dim!=1 or nik!=1
+    if( st%d%nik.ne.1.or.st%d%dim.ne.1) call messages_not_implemented("SCDM with k-points or dims")
+    ! gather states in case of domain paralleization
+    if(mesh%parallel_in_domains) then
+       SAFE_ALLOCATE(state_global(1:mesh%np_global))
+       !
+       do i=1,nval
+          ! KSt(i,:) = st%dpsi(:,st%d%dim,i,st%d%nik)
+          call X(vec_gather)(mesh%vp, 0, state_global, st%X(psi)(1:mesh%np,st%d%dim,i,st%d%nik))
+          if(scdm%root) KSt(i,:)  = R_CONJ(state_global(:))
+       enddo
+       SAFE_DEALLOCATE_A(state_global)
+    else
+       ! serial
+       SAFE_ALLOCATE(temp_state(1:mesh%np,1))
+       do i=1,nval
+          ! this call is necessary becasue we want to have only np not np_part
+! this needs to be complex in td
+           call states_get_state(st, mesh, i, st%d%nik, temp_state)
+           KSt(i,:) = temp_state(:,1)!st%dpsi(:,st%d%dim,i,st%d%nik)
+       enddo
+       SAFE_DEALLOCATE_A(temp_state)
+    endif
+    !
+    ! possibly redundant copy
+    if(scdm%root) KSt_original(:,:) = KSt(:,:)
+    !
+    !
+call cpu_time(t1)
+    ! perform the RRQR
+    scdm%st%X(psi)(:,:,:,:) = M_ZERO ! this is important for distribution later
+!---! ----------------------------- SERIAL START --------------------------------
+    if(scdm%root) then
+       !
+       call X(RRQR)(nval,mesh%np_global,KSt,JPVT)
+       call cpu_time(t2)
+       print *, 'time: RRQR:', t2-t1
+       !
+       SAFE_DEALLOCATE_A(KSt)
+       !
+       ! form SCDM Note: This could be done in one step together with the orhtogonalization
+       !                 to save this allocation
+       SAFE_ALLOCATE(SCDM_temp(mesh%np_global,nval))
+       SCDM_temp(:,:) = 0.
+       do i=1,nval
+          do v=1,nval
+             SCDM_temp(:,i) = SCDM_temp(:,i) + KSt_original(v,:)*R_CONJ(KSt_original(v,JPVT(i)))
+!             SCDM_temp(:,i) = SCDM_temp(:,i) + st%dpsi(:,st%d%dim,v,scdm%st%d%nik)*st%dpsi(JPVT(i),st%d%dim,v,scdm%st%d%nik)
+          enddo
+       enddo
+       !
+       call cpu_time(t1)
+       print *, 'time: explicit matmul1:',t1-t2
+       !
+       ! --- Orthogoalization ----
+       ! form lower trinagle of Pcc
+       SAFE_ALLOCATE(Pcc(nval,nval))
+       Pcc(:,:) = 0.
+       do i=1,nval
+          do j=1,i
+             do v=1,nval
+                Pcc(i,j) = Pcc(i,j)+ KSt_original(v,JPVT(i))*R_CONJ(KSt_original(v,JPVT(j)))
+!                Pcc(i,j) = Pcc(i,j)+ st%dpsi(JPVT(i),st%d%dim,v,scdm%st%d%nik)*st%dpsi(JPVT(j),st%d%dim,v,scdm%st%d%nik)
+             enddo
+          enddo
+       enddo
+       !
+       call cpu_time(t2)
+       print *, 'time: explicit matmul2:',t2-t1
+       ! Cholesky fact.
+       call X(POTRF)("L", nval, Pcc, nval, INFO )
+       if(INFO.ne.0) then
+          if(INFO.lt.0) then
+             print *, 'Illegal argument in DPOTRF: ', INFO
+          else
+             print *, 'Fail of Cholesky, not pos-semi-def '
+          endif
+          stop
+       endif
+       !
+       call cpu_time(t1)
+       print *, 'time: cholesky:',t1-t2
+       ! transpose
+       Pcc(:,:) = transpose(R_CONJ(Pcc(:,:)))
+       ! invert
+       call X(invert)(nval,Pcc)
+       !
+       call cpu_time(t2)
+       print *, 'time: transpose invert:',t2-t1
+       ! form ortho SCDM
+       scdm%st%X(psi)(:,:,:,:) = M_ZERO
+       do i=1,mesh%np_global
+          do j=1,nval
+             do v=1,nval
+                scdm%st%X(psi)(i,1,v,1) = scdm%st%X(psi)(i,1,v,1) + SCDM_temp(i,j)*Pcc(j,v)
+             enddo
+          enddo
+       enddo
+       !
+       SAFE_DEALLOCATE_A(SCDM_temp)
+       call cpu_time(t1)
+       print *, 'time: explicit matmul3',t1-t2
+       ! normalise SCDM states
+       do v=1,nval
+          scdm%st%X(psi)(:,1,v,1) = scdm%st%X(psi)(:,1,v,1)/ &
+               sqrt(dot_product(scdm%st%X(psi)(:,1,v,1),scdm%st%X(psi)(:,1,v,1))* mesh%volume_element)
+       enddo
+       call cpu_time(t2)
+       print *, 'time: norms',t2-t1
+       !
+! write cube files
+!do v=1,10
+!  write(name,'(I10)') v
+!  name = 'scdm_'//trim(adjustl(name))
+!  !enddo
+!call dio_function_output (io_function_fill_how('Cube'), ".", "HF_1", mesh, st%dpsi(:,1,1,1), unit_one, info,geo=scdm_geo)
+       call cpu_time(t1)
+       print *, 'time: output',t1-t2
+       !
+       ! find centers, by computing center of mass of |psi|^2
+       scdm%center(:,:) = 0
+       do v=1,nval
+          do i=1,3
+!             scdm%center(i,v) = sum(scdm%st%dpsi(:,st%d%dim,v,scdm%st%d%nik)**2*mesh%x(:,i))*mesh%volume_element
+             scdm%center(i,v) = sum(scdm%st%X(psi)(:,st%d%dim,v,scdm%st%d%nik)*R_CONJ(scdm%st%X(psi)(:,st%d%dim,v,scdm%st%d%nik))* &
+                       mesh%idx%lxyz(:,i)*mesh%spacing(i))*mesh%volume_element
+          enddo
+          write(127,*) scdm%center(:,v)
+       enddo
+       call cpu_time(t2)
+       print *, 'time: find centers',t2-t1
+    endif
+    !
+!---! --------------------- SERIAL END ------------------------------------
+    call MPI_Barrier(mesh%mpi_grp%comm, mpi_err)
+   !
+    ! distribute the localized states
+    count = 0
+    SAFE_ALLOCATE(temp_state(1:mesh%np_global,1))
+    temp_state(:,:) = M_ZERO
+    do i=1,nval
+       ! send state to all processes as temp
+       ! this is only non-zero on root
+       temp_state(:,1) =  scdm%st%X(psi)(1:mesh%np_global,st%d%dim,i,scdm%st%d%nik)
+       call MPI_Bcast(temp_state(1,1),mesh%np_global , R_MPITYPE, 0, mesh%mpi_grp%comm, mpi_err)
+       !
+       ! only keep the state if it falls into index range on process
+       if(i.ge.scdm%st_start.and.i.le.scdm%st_end) then
+          count = count +1
+          scdm%st%X(psi)(1:mesh%np_global,st%d%dim,count,scdm%st%d%nik) = temp_state(:,1)
+       endif
+       !
+    enddo
+    SAFE_DEALLOCATE_A(temp_state)
+    !
+    ! broadcast the centers to all processes
+    call MPI_Bcast(scdm%center(1,1),size(scdm%center) ,MPI_FLOAT, 0, mesh%mpi_grp%comm, mpi_err)
+    !
+    ! copy local box of state
+call cpu_time(t1)
+   count = 0
+   scdm%X(psi)(:,:) =  M_ZERO
+   do v=scdm%st_start,scdm%st_end
+      count = count +1
+       ! find integer index of center
+       do i=1,3
+          icenter(i) = scdm%center(i,v)/mesh%spacing(i)
+       enddo
+       ! find index of center in the mesh
+       ind_center = mesh%idx%lxyz_inv(icenter(1),icenter(2),icenter(3))
+       !
+       ! make list with points in the box
+       scdm%box(:,:,:,count) =  mesh%idx%lxyz_inv(icenter(1)-scdm%box_size:icenter(1)+scdm%box_size, &
+                                              icenter(2)-scdm%box_size:icenter(2)+scdm%box_size, &
+                                              icenter(3)-scdm%box_size:icenter(3)+scdm%box_size)
+
+       !
+       ! copy points to box
+       ! this box refers to the global mesh
+       do j=1,scdm%box_size*2+1
+          do k=1,scdm%box_size*2+1
+             do l=1,scdm%box_size*2+1
+                ! map into the twice larger box
+                ip = (j-1)*(2*(scdm%box_size*2+1))**2+(k-1)*(2*(scdm%box_size*2+1)) + l
+                scdm%X(psi)(ip,count) = scdm%st%X(psi)(scdm%box(j,k,l,count),st%d%dim,count,scdm%st%d%nik)
+             enddo
+          enddo
+       enddo
+       !
+! compue localization error
+print *, 'state, localization error', v, 1.-dot_product(scdm%X(psi)(:,count),scdm%X(psi)(:,count))*mesh%volume_element
+! f localization eror set zero INside the box
+!do j=1,scdm%box_size*2+1
+!   do k=1,scdm%box_size*2+1
+!      do l=1,scdm%box_size*2+1
+!        ip = (j-1)*(2*(scdm%box_size*2+1))**2+(k-1)*(2*(scdm%box_size*2+1)) + l
+!        scdm%st%X(psi)(scdm%box(j,k,l,v),st%d%dim,v,scdm%st%d%nik) = M_ZERO
+!     enddo
+!  enddo
+!enddo
+!print *, 'state, localization error', v, real(dot_product(scdm%st%X(psi)(:,1,count,1),scdm%st%X(psi)(:,1,count,1))*mesh%volume_element)
+
+       call MPI_Barrier(mesh%mpi_grp%comm, mpi_err)
+       !
+! for testing zero outside the box
+scdm%st%X(psi)(:,st%d%dim,v,scdm%st%d%nik) = 0.
+do j=1,scdm%box_size*2+1
+   do k=1,scdm%box_size*2+1
+      do l=1,scdm%box_size*2+1
+         ip = (j-1)*(2*(scdm%box_size*2+1))**2+(k-1)*(2*(scdm%box_size*2+1)) + l
+        scdm%st%X(psi)(scdm%box(j,k,l,v),st%d%dim,v,scdm%st%d%nik) = scdm%X(psi)(ip,count)
+     enddo
+  enddo
+enddo
+
+       !
+   enddo
+   !
+call cpu_time(t2)
+print *, 'time: copy box',t2-t1
+
+    ! set flag to do this only once
+    scdm_is_local = .true.
+    !
+    !
+    SAFE_DEALLOCATE_A(Pcc)
+    SAFE_DEALLOCATE_A(KSt)
+    SAFE_DEALLOCATE_A(scdm_temp)
+    !
+    print *, 'HH: done SCDM localize'
+    !
+call cpu_time(t1)
+print *, 'time: all SCDM',t1-t0
+    !
+return
+!if(scdm%iter.le.20) return
+    ! calculate exchaneg energy
+    SAFE_ALLOCATE(rho(1:mesh%np_part))
+    SAFE_ALLOCATE(rho2(1:mesh%np_part))
+    SAFE_ALLOCATE(pot(1:mesh%np_part))
+    !
+    exx = 0.
+    do i=1,nval
+       do j=1,i
+          temp(:) = (scdm%center(:,i)- scdm%center(:,j))/mesh%spacing(:)
+          if(sqrt(dot_product(temp,temp)).le.2.*scdm%rcut) then
+             !
+             rho(:) = scdm%st%X(psi)(:,st%d%dim,i,scdm%st%d%nik)*scdm%st%X(psi)(:,st%d%dim,j,scdm%st%d%nik)
+!rho(:) = st%dpsi(:,st%d%dim,i,scdm%st%d%nik)*st%dpsi(:,st%d%dim,j,scdm%st%d%nik)
+             rho2(:) = rho(:)
+             pot(:) = 0.
+             call dpoisson_solve(psolver, pot, rho, all_nodes = .false.)
+             !
+             ! catch diagonals for double counting
+             if(i.ne.j) then
+                exx = exx - 0.5*dot_product(pot(:),rho2(:))*mesh%volume_element
+             else
+                exx = exx - 0.25*dot_product(pot(:),rho2(:))*mesh%volume_element
+             endif
+          endif
+          !
+       enddo
+    enddo
+    !
+    print *, 'HH: exx[eV] = ', exx*27.211396132
+    !
+  end subroutine X(scdm_localize)
+
+
+ subroutine X(invert)(n,A)
+    ! stupid routine to invert with LAPACK
+    ! is very redundant here, shoudl be replaced by something smart
+    integer         ::  n
+    R_TYPE          ::  A(n,n)
+    integer         :: ierror,ipiv(n), lwork
+    R_TYPE,pointer  :: work(:)
+    FLOAT           :: temp
+    !
+    call X(getrf)( n, n, A, n, ipiv, ierror )
+    if( ierror.eq.0 ) then
+       !workspace query
+       call X(getri)( n, A, n, ipiv, temp, -1, ierror )
+       lwork = temp ! dimension of workspace
+       allocate(work(lwork*2))
+       call X(getri)( n, A, n, ipiv, work, lwork, ierror )
+    else
+       stop 'Terminating due to failed LU decomp'
+    endif
+    if (ierror.ne.0) then
+       stop 'Terminating due to failed inversion'
+    endif
+    deallocate(work)
+    !
+  end subroutine X(invert)
